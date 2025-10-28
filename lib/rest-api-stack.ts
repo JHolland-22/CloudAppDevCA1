a1dd7f5cf4305a05ad5daf65c385c061427410a1
JHolland <20102340@mail.wit.ie>
2025-10-28 18:01:04 +0000

    added in deleting and fixed the structure of the rest-api-stack

--- lib/rest-api-stack.ts
+++ lib/rest-api-stack.ts
@@ -73,10 +73,41 @@ export class RestAPIStack extends cdk.Stack {
             resources: [moviesTable.tableArn],
           }),
         });
+
+
+
+    const newMovieFn = new lambdanode.NodejsFunction(this, "AddMovieFn", {
+      architecture: lambda.Architecture.ARM_64,
+      runtime: lambda.Runtime.NODEJS_16_X,
+      entry: `${__dirname}/../lambdas/addMovie.ts`,
+      timeout: cdk.Duration.seconds(10),
+      memorySize: 128,
+      environment: {
+        TABLE_NAME: moviesTable.tableName,
+        REGION: cdk.Aws.REGION,
+      },
+    });
+
+
+    const deleteMovieFn = new lambdanode.NodejsFunction(this, "DeleteMovieFn", {
+      architecture: lambda.Architecture.ARM_64,
+      runtime: lambda.Runtime.NODEJS_16_X,
+      entry: `${__dirname}/../lambdas/addMovie.ts`,
+      timeout: cdk.Duration.seconds(10),
+      memorySize: 128,
+      environment: {
+        TABLE_NAME: moviesTable.tableName,
+        REGION: cdk.Aws.REGION,
+      },
+    });
         
         // Permissions 
         moviesTable.grantReadData(getMovieByIdFn)
         moviesTable.grantReadData(getAllMoviesFn)
+        moviesTable.grantReadWriteData(newMovieFn)
+        moviesTable.grantReadWriteData(deleteMovieFn)
+
+
 
 
 
@@ -96,20 +127,8 @@ export class RestAPIStack extends cdk.Stack {
     });
 
 
-    const newMovieFn = new lambdanode.NodejsFunction(this, "AddMovieFn", {
-      architecture: lambda.Architecture.ARM_64,
-      runtime: lambda.Runtime.NODEJS_16_X,
-      entry: `${__dirname}/../lambdas/addMovie.ts`,
-      timeout: cdk.Duration.seconds(10),
-      memorySize: 128,
-      environment: {
-        TABLE_NAME: moviesTable.tableName,
-        REGION: cdk.Aws.REGION,
-      },
-    });
         
-    moviesTable.grantReadWriteData(newMovieFn)
-
+    
 const moviesEndpoint = api.root.addResource("movies");
 
 moviesEndpoint.addMethod(
@@ -127,6 +146,12 @@ const movieEndpoint = moviesEndpoint.addResource("{movieId}");
 movieEndpoint.addMethod(
   "GET",
   new apig.LambdaIntegration(getMovieByIdFn, { proxy: true })
+);
+
+
+movieEndpoint.addMethod(
+  "DELETE",
+  new apig.LambdaIntegration(deleteMovieFn, { proxy: true })
 );
            
       }
