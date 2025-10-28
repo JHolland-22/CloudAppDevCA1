c1d4ccf5c00a90b161942e681d8aaf4287d24319
JHolland <20102340@mail.wit.ie>
2025-10-28 22:37:06 +0000

    setup for Auth APII

--- lib/cognito-stack.ts
+++ lib/cognito-stack.ts
@@ -15,6 +15,9 @@ export class CognitoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+
+
+
     const userPool = new UserPool(this, "UserPool", {
       signInAliases: { username: true, email: true },
       selfSignUpEnabled: true,
@@ -37,6 +40,38 @@ export class CognitoStack extends cdk.Stack {
       },
     });
 
+
+    
     this.auth = authApi.root.addResource("auth");
   }
+
+
+    private addAuthRoute(
+    resourceName: string,
+    method: string,
+    fnName: string,
+    fnEntry: string,
+    allowCognitoAccess?: boolean
+  ): void {
+    const commonFnProps = {
+      architecture: lambda.Architecture.ARM_64,
+      timeout: cdk.Duration.seconds(10),
+      memorySize: 128,
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: "handler",
+      environment: {
+        USER_POOL_ID: this.userPoolId,
+        CLIENT_ID: this.userPoolClientId,
+        REGION: cdk.Aws.REGION
+      },
+    };
+    const resource = this.auth.addResource(resourceName);
+    
+    const fn = new node.NodejsFunction(this, fnName, {
+      ...commonFnProps,
+      entry: `${__dirname}/../lambda/auth/${fnEntry}`,
+    });
+
+    resource.addMethod(method, new apig.LambdaIntegration(fn));
+  }  // end private method
 }
